3eb4d958d158959af12d0e613b6acfeb211f0c9e
Diogo Manuel Faria Martins <dmanuel-@c1r9s13.42porto.com>
2023-03-27 12:32:40 +0100

    Fixed??????????????

--- src/main.c
+++ src/main.c
@@ -26,19 +26,18 @@ static void	free_z(int **z)
 
 static void	clear_win(t_fdf *data)
 {
-	mlx_clear_window(data->mlx_ptr, data->win_ptr);
-	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	//mlx_clear_window(data->mlx_ptr, data->win_ptr);
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);;
 	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
 	free(data);
 }
 
-static int	close_win(int keycode, void *param)
+static int	close_win(t_fdf *data)
 {
-	if (keycode == 65307 || (int *)param == (int *)347)
-	{
-		//clear_win(data);
-		exit(0);
-	}
+	free_z(data->z_pos);
+	clear_win(data);
+	exit(0);
 	return (0);
 }
 
@@ -55,18 +54,19 @@ static int	handle_input(int keysym, t_fdf *data)
 
 int	main(int argc, char **argv)
 {
-	t_fdf	*data;
+	if (argc == 2)
+	{
+		t_fdf	*data;
 
-	(void) argc;
-	data = (t_fdf *)malloc(sizeof(t_fdf));
-	read_file(argv[1], data);
-	data->zoom = 5;
-	data->mlx_ptr = mlx_init();
-	data->win_ptr = mlx_new_window(data->mlx_ptr, 1000, 1000, "FDF");
-	draw(data);
-	mlx_key_hook(data->win_ptr, &handle_input, data);
-	mlx_hook(data->win_ptr, 17, 1L << 17, close_win, NULL);
-	mlx_loop(data->mlx_ptr);
-	free_z(data->z_pos);
-	clear_win(data);
+		(void) argc;
+		data = (t_fdf *)malloc(sizeof(t_fdf));
+		read_file(argv[1], data);
+		data->zoom = 5;
+		data->mlx_ptr = mlx_init();
+		data->win_ptr = mlx_new_window(data->mlx_ptr, 800, 800, "FDF");
+		draw(data);
+		mlx_key_hook(data->win_ptr, &handle_input, data);
+		mlx_hook(data->win_ptr, 17, 1L << 17, close_win, data);
+		mlx_loop(data->mlx_ptr);
+	}
 }
